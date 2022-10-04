ed9402d1f541142c7d7a945b401dc500d8af064d
k-renj <gsp.karen@gmail.com>
2022-10-03 21:04:33 -0600

    03/10

--- KAPA/app/src/main/java/com/krevolorio/myappkapa/MainActivity.java
+++ KAPA/app/src/main/java/com/krevolorio/myappkapa/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -48,6 +49,21 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME);
     }
 
+    private void aperturaLogin(){
+    Intent intent = new Intent(this, MAlogin.class);
+    startActivity(intent);
+    }
+
+    private void aperturaComprar(){
+        Intent intent = new Intent(this, MAcomprar.class);
+        startActivity(intent);
+    }
+
+    private void aperturaDetallecompra(){
+        Intent intent = new Intent(this, MAdetallecompra.class);
+        startActivity(intent);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -61,12 +77,15 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.login:
                 Toast.makeText(this, "Ir a Login", Toast.LENGTH_SHORT).show();
+                aperturaLogin();
                 break;
             case R.id.comprar:
                 Toast.makeText(this, "Necesita hacer login", Toast.LENGTH_SHORT).show();
+                aperturaComprar();
                 break;
             case R.id.detalleCompra:
                 Toast.makeText(this, "Ver detalle de compra", Toast.LENGTH_SHORT).show();
+                aperturaDetallecompra();
                 break;
         }
         return super.onOptionsItemSelected(item);
